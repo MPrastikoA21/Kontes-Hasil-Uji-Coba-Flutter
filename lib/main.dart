9558bdb015525c37f864951e6a9fc015f3389221
Marcel Prastiko Arthana <marcel@undiksha.ac.id>
2021-03-16 22:12:00 +0800

    Marcel Prastiko Arthana

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lightGreenAccent[700],
+        appBar: AppBar(
+          leading: Icon(Icons.home),
+          title: Text('Flutter Tutorial'),
+          actions: <Widget>[
+            Icon(Icons.search),
+          ],
+          elevation: 0,
+          backgroundColor: Colors.cyanAccent[400],
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://sso.undiksha.ac.id/cas/assets/images/sistem/2207_logo.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
